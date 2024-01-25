bce9dc93ae7c872a69cd50cf9aa3bc3931f71275
박정호 <cuzzzu1318@naver.com>
2024-01-25 12:42:17 +0900

    feat: #259 근황 글 작성 - 컨트롤러 수정

--- Back/src/main/java/com/e202/dogcatdang/board/controller/BoardController.java
+++ Back/src/main/java/com/e202/dogcatdang/board/controller/BoardController.java
@@ -1,12 +1,17 @@
 package com.e202.dogcatdang.board.controller;
 
+import java.io.IOException;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.e202.dogcatdang.board.dto.ImageResponseDto;
 import com.e202.dogcatdang.board.dto.WriteBoardDto;
@@ -22,7 +27,12 @@ public class BoardController {
 	private final BoardService boardService;
 
 	@PostMapping("")
-	public ResponseEntity write(@RequestBody WriteBoardDto writeBoardDto) {
+	public ResponseEntity write(@RequestPart WriteBoardDto writeBoardDto,
+		@RequestPart("file") List<MultipartFile> files) throws IOException {
+
+		writeBoardDto.setImageList(files);
+
+		System.out.println("writeBoardDto = " + writeBoardDto);
 		boardService.save(writeBoardDto);
 		return new ResponseEntity(HttpStatus.OK);
 	}
