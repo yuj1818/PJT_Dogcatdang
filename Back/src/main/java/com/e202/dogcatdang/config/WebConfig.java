a653e63a2ac3b542ff204fcf8e87e201bef4bb3b
-l <cuzzzu1318@naver.com>
2024-01-27 02:03:04 +0900

    temp: CORS 임시수정

--- Back/src/main/java/com/e202/dogcatdang/config/WebConfig.java
+++ Back/src/main/java/com/e202/dogcatdang/config/WebConfig.java
@@ -13,10 +13,11 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("*")
+			.allowedOriginPatterns("*")
 			.allowedMethods("GET", "POST", "PUT", "DELETE")
 			.allowedHeaders("Authorization", "Content-Type")
 			.exposedHeaders("Custom-Header")
+			.allowCredentials(true)
 			.maxAge(3600);
 	}
 }
