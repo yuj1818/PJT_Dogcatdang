241d4fe9b0929b59428c06e10517b07f28036a3a
탁윤희 <gwalgwal98@naver.com>
2024-02-05 16:26:30 +0900

    fix: #498 방문 예약 @Entity 추가

--- Back/src/main/java/com/e202/dogcatdang/db/entity/Reservation.java
+++ Back/src/main/java/com/e202/dogcatdang/db/entity/Reservation.java
@@ -3,6 +3,7 @@ package com.e202.dogcatdang.db.entity;
 import java.time.LocalDateTime;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
@@ -13,6 +14,7 @@ import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
+@Entity
 @Table(name = "reservation")
 public class Reservation {
 
