86f7ec87e5b53abcb0790c95d7dd062197bf7c14
위동민 <dod212@naver.com>
2024-02-05 17:54:29 +0900

    feat: #576 OAuth2.0 수정

---
+++ Back/src/main/java/com/e202/dogcatdang/oauth2/dto/OauthUserDTO.java
@@ -0,0 +1,14 @@
+package com.e202.dogcatdang.oauth2.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class OauthUserDTO {
+
+    private String phone;
+    private String nickname;
+
+}
