8b52320f6a87eb4d9e78f963ce9dec15368cbdd2
Dohun-choi <139415969+Dohun-choi@users.noreply.github.com>
2024-02-07 17:56:40 +0900

    fix: 안쓰는 변수 제거

--- Front/src/pages/articles/ArticleListPage.tsx
+++ Front/src/pages/articles/ArticleListPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useQuery, QueryFunctionContext } from "@tanstack/react-query";
 
 import TextSearch from "../../components/common/TextSearch";
