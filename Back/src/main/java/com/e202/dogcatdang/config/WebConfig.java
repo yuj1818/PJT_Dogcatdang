ac9ae692a8c9faa2fe79a9eb83bf77e8ff3002f0
박정호 <cuzzzu1318@naver.com>
2024-02-08 12:38:20 +0900

    Infra: fix

--- Back/src/main/java/com/e202/dogcatdang/config/WebConfig.java
+++ Back/src/main/java/com/e202/dogcatdang/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.e202.dogcatdang.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
