459d27777c0601a4e8988032a688faba78b7201b
박정호 <cuzzzu1318@naver.com>
2024-02-08 11:28:09 +0900

    Infra

--- Front/vite.config.ts
+++ Front/vite.config.ts
@@ -1,14 +1,14 @@
-import { loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
-import { createHtmlPlugin } from "vite-plugin-html";
+import { loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import { createHtmlPlugin } from 'vite-plugin-html'
 
 // https://vitejs.dev/config/
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd());
+  console.log(env)
 
-  return defineConfig({
-    base: '/',
+  return {
     plugins: [
       react(),
       createHtmlPlugin({
@@ -16,9 +16,9 @@ export default ({ mode }) => {
         inject: {
           data: {
             kakaoMapApiKey: env.VITE_KAKAO_MAP_API_KEY,
-          },
-        },
-      }),
+          }
+        }
+      })
     ],
-  };
-};
+  }
+}
