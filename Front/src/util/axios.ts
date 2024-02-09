50bb2689a4a27d3d67d2e6cbc6bf923fae681352
박정호 <cuzzzu1318@naver.com>
2024-02-09 15:44:04 +0900

    Infra: 백엔드 https

--- Front/src/util/axios.ts
+++ Front/src/util/axios.ts
@@ -3,11 +3,11 @@ import axios from "axios";
 const env = import.meta.env.VITE_IS_PRODUCTION || "development";
 export const isProduction = env === "production";
 
-export const HOST = isProduction ? "https://i10e202.p.ssafy.io" : "https://localhost";
+export const HOST = isProduction ? "https://i10e202.p.ssafy.io" : "http://localhost";
 
 export const PORT = ":8443";
 
-export const URL = HOST + PORT;
+export const URL = isProduction? HOST : HOST + PORT;
 
 const API = axios.create({
   baseURL: URL,
