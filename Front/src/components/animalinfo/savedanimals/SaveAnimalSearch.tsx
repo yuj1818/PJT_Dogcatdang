5fbb9922c0ca5d8dfe556b2116bc779ae3f91ebf
박영진 <dud_wls_95@naver.com>
2024-02-12 22:59:15 +0900

    style: filter CSS 추가

--- Front/src/components/animalinfo/savedanimals/SaveAnimalSearch.tsx
+++ Front/src/components/animalinfo/savedanimals/SaveAnimalSearch.tsx
@@ -13,6 +13,7 @@ import { Input, Select as Select1 } from "../../../components/animalinfo/style";
 import { Cookies } from "react-cookie";
 import { search, FilterData } from "../../../util/SaveAPI";
 import { useNavigate } from "react-router-dom";
+import { Title } from "../../common/Title";
 
 export interface AnimalType {
   animalId: number;
@@ -47,8 +48,7 @@ const AnimalButton = styled.button<{ selected: boolean }>`
   ${(props) =>
     props.selected &&
     css`
-      filter: blur(1px);
-      opacity: 0.9;
+      opacity: 0.5;
     `};
 `;
 
@@ -121,143 +121,156 @@ function SaveAnimalSearch() {
   }
 
   return (
-    <div className="container">
-      <img
-        src={SearchImg}
-        alt="search"
-        style={{
-          position: "absolute",
-          right: 90,
-          top: 0,
-          width: "70px",
-          height: "70px",
-        }}
-      ></img>
-      <form className="search-form" onSubmit={handleSearch}>
-        <div className="button-group">
-          <AnimalButton
-            selected={animalType === "고양이"}
-            onClick={() => handleAnimalType("강아지")}
-            name="animalType"
-            type="button"
-          >
-            강아지
-          </AnimalButton>
-          <AnimalButton
-            selected={animalType === "강아지"}
-            onClick={() => handleAnimalType("고양이")}
-            name="animalType"
-            type="button"
-          >
-            고양이
-          </AnimalButton>
-        </div>
-        <div className="form-group">
-          <Select
-            name="breed"
-            id="breed"
-            value={
-              animalType === "강아지"
-                ? transformedDogInput.find((option) => option.value === breed)
-                : transformedCatInput.find((option) => option.value === breed)
-            }
-            options={
-              animalType === "강아지"
-                ? transformedDogInput
-                : transformedCatInput
-            }
-            onChange={(selectedOption) => setBreed(selectedOption?.value || "")}
-            placeholder="품종"
-            styles={{
-              control: (provided) => ({
-                ...provided,
-                border: "1px solid #d5967b",
-                padding: "8px",
-                borderRadius: "10px",
-                width: "160px",
-                height: "60px",
-              }),
-            }}
-          />
-        </div>
-        <div className="form-group">
-          <Select1
-            name="region"
-            id="region"
-            value={region}
-            onChange={handleRegionChange}
-            className="custom-input"
-          >
-            <option value="" disabled hidden>
-              시/도 선택
-            </option>
-            {regionInput.map((pr) => (
-              <option key={pr} value={pr}>
-                {pr}
-              </option>
-            ))}
-          </Select1>
-        </div>
-        <div className="form-group">
-          <Select1
-            name="country"
-            id="country"
-            value={country}
-            onChange={handleCountryChange}
-            className="custom-input"
-          >
-            <option value="" disabled hidden>
-              시/구/군 선택
-            </option>
-            {countryInput[regionInput.indexOf(region)] &&
-              countryInput[regionInput.indexOf(region)].map((ct, index) => (
-                <option key={index} value={ct}>
-                  {ct}
+    <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-3">
+        <Title className="title">보호 동물 조회</Title>
+        <hr className="border-black" />
+      </div>
+      <div className="container">
+        <img
+          src={SearchImg}
+          alt="search"
+          style={{
+            position: "absolute",
+            right: 90,
+            top: 0,
+            width: "70px",
+            height: "70px",
+          }}
+        ></img>
+        <form className="search-form" onSubmit={handleSearch}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+
+            <div>
+              <div className="button-group">
+                <AnimalButton
+                  selected={animalType === "고양이"}
+                  onClick={() => handleAnimalType("강아지")}
+                  name="animalType"
+                  type="button"
+                >
+                  강아지
+                </AnimalButton>
+                <AnimalButton
+                  selected={animalType === "강아지"}
+                  onClick={() => handleAnimalType("고양이")}
+                  name="animalType"
+                  type="button"
+                >
+                  고양이
+                </AnimalButton>
+              </div>
+            </div>
+            <div style={{ display: 'flex', flexDirection: 'row' }}>
+              <div className="form-group">
+                <Select
+                  name="breed"
+                  id="breed"
+                  value={
+                    animalType === "강아지"
+                      ? transformedDogInput.find((option) => option.value === breed)
+                      : transformedCatInput.find((option) => option.value === breed)
+                  }
+                  options={
+                    animalType === "강아지"
+                      ? transformedDogInput
+                      : transformedCatInput
+                  }
+                  onChange={(selectedOption) => setBreed(selectedOption?.value || "")}
+                  placeholder="품종"
+                  styles={{
+                    control: (provided) => ({
+                      ...provided,
+                      border: "1px solid #d5967b",
+                      padding: "8px",
+                      borderRadius: "10px",
+                      width: "160px",
+                      height: "60px",
+                    }),
+                  }}
+                />
+              </div>
+              <div className="form-group">
+                <Select1
+                  name="region"
+                  id="region"
+                  value={region}
+                  onChange={handleRegionChange}
+                  className="custom-input"
+                >
+                  <option value="" disabled hidden>
+                    시/도 선택
+                  </option>
+                  {regionInput.map((pr) => (
+                    <option key={pr} value={pr}>
+                      {pr}
+                    </option>
+                  ))}
+                </Select1>
+              </div>
+              <div className="form-group">
+                <Select1
+                  name="country"
+                  id="country"
+                  value={country}
+                  onChange={handleCountryChange}
+                  className="custom-input"
+                >
+                  <option value="" disabled hidden>
+                    시/구/군 선택
+                  </option>
+                  {countryInput[regionInput.indexOf(region)] &&
+                    countryInput[regionInput.indexOf(region)].map((ct, index) => (
+                      <option key={index} value={ct}>
+                        {ct}
+                      </option>
+                    ))}
+                </Select1>
+              </div>
+              <div className="form-group">
+                <option value="" disabled hidden>
+                  성별
                 </option>
-              ))}
-          </Select1>
-        </div>
-        <div className="form-group">
-          <option value="" disabled hidden>
-            성별
-          </option>
-          <Select1
-            name="gender"
-            id="gender"
-            value={gender}
-            onChange={handleGenderChange}
-            className="custom-input"
-          >
-            <option value="" disabled hidden>
-              성별
-            </option>
-            {genderInput.map((ge) => (
-              <option key={ge} value={ge}>
-                {ge}
-              </option>
-            ))}
-          </Select1>
-        </div>
-        <div className="form-group">
-          <Input
-            type="text"
-            id="shelterName"
-            name="shelterName"
-            value={shelterName}
-            onChange={handleShelterNameChange}
-            placeholder="보호기관명"
-            className="custom-input"
-          />
-        </div>
-        <div className="form-group">
-          <button
-            className="search-button"
-            type="submit"
-          >
-            검색
-          </button>
-        </div>
-      </form>
+                <Select1
+                  name="gender"
+                  id="gender"
+                  value={gender}
+                  onChange={handleGenderChange}
+                  className="custom-input"
+                >
+                  <option value="" disabled hidden>
+                    성별
+                  </option>
+                  {genderInput.map((ge) => (
+                    <option key={ge} value={ge}>
+                      {ge}
+                    </option>
+                  ))}
+                </Select1>
+              </div>
+              <div className="form-group">
+                <Input
+                  type="text"
+                  id="shelterName"
+                  name="shelterName"
+                  value={shelterName}
+                  onChange={handleShelterNameChange}
+                  placeholder="보호기관명"
+                  className="custom-input"
+                />
+              </div>
+              <div className="form-group">
+                <button
+                  className="search-button"
+                  type="submit"
+                >
+                  검색
+                </button>
+              </div>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
