fc14926445f8106912ab7953a7e7aed7670c5782
박정호 <cuzzzu1318@naver.com>
2024-02-13 23:21:20 +0900

    fix: 방송 시작 시 관심동물 등록한 유저 알림 전송

--- Back/src/main/java/com/e202/dogcatdang/streaming/service/StreamingServiceImpl.java
+++ Back/src/main/java/com/e202/dogcatdang/streaming/service/StreamingServiceImpl.java
@@ -8,9 +8,14 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.e202.dogcatdang.db.entity.Animal;
+import com.e202.dogcatdang.db.entity.AnimalLike;
+import com.e202.dogcatdang.db.entity.Notification;
 import com.e202.dogcatdang.db.entity.Streaming;
 import com.e202.dogcatdang.db.entity.StreamingAnimal;
 import com.e202.dogcatdang.db.entity.User;
+import com.e202.dogcatdang.db.repository.AnimalLikeRepository;
+import com.e202.dogcatdang.db.repository.NotificationRepository;
 import com.e202.dogcatdang.db.repository.StreamingAnimalRepository;
 import com.e202.dogcatdang.db.repository.StreamingRepository;
 import com.e202.dogcatdang.db.repository.UserRepository;
@@ -28,13 +33,33 @@ public class StreamingServiceImpl implements StreamingService{
 	private final StreamingRepository streamingRepository;
 	private final UserRepository userRepository;
 	private final StreamingAnimalRepository streamingAnimalRepository;
+	private final AnimalLikeRepository animalLikeRepository;
+	private final NotificationRepository notificationRepository;
 
 	@Override
+	@Transactional
 	public ResponseDto startStreaming(Long loginUserId, RequestStreamingDto requestStreamingDto) {
 		User loginUser = userRepository.findById(loginUserId).get();
 
 		List<StreamingAnimal> animalList = new ArrayList<>();
+		
+		//동물 id 리스트에서 해당 동물을 즐겨찾기한 유저에 대해 알림 전송
 		for(Long id : requestStreamingDto.getAnimalInfo()){
+			List<AnimalLike> animalLikeList = animalLikeRepository.findByAnimalAnimalId(id);
+			for (AnimalLike animalLike : animalLikeList) {
+				User receiver = animalLike.getUser();
+				User sender = animalLike.getAnimal().getUser();
+				String title = animalLike.getAnimal().getCode()+"에 대한 방송이 시작되었습니다.";
+				String content = "<a href=\"http://localhost:5173/\">방송 바로가기(임시로 메인으로 가게 헀습니다.)</a>";
+				Notification notification = Notification.builder()
+					.receiver(receiver)
+					.sender(sender)
+					.title(title)
+					.content(content)
+					.build();
+				notificationRepository.save(notification);
+			}
+
 			animalList.add(streamingAnimalRepository.findByAnimalAnimalId(id));
 		}
 
